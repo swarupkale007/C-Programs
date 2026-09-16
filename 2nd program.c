d48b60b17104d789f87cfef6cca4d70a43334240
swarupkale007 <swarupkale16@gmail.com>
2026-09-16 14:13:11 +0530

    Add basic program to greet user by name

---
+++ 2nd program.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    char name[20];
+    clrscr();
+    printf("Enter Your Name: ");
+    scanf("%s",name);
+    printf("Hello Robotics!\n");
+    printf("Welcome %s\n",name);
+    getch();
+}
